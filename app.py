df64fa5cc49bbbcf8ad35109f4cdae97710d0f76
kenneith1024@gmail.com <Ken1024ken>
2022-05-16 18:17:08 +0800

    Go!

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import os
+import requests
+import json
+
 from datetime import datetime
 
 from flask import Flask, abort, request
@@ -34,7 +37,26 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     get_message = event.message.text
+    
+    url = "https://aispapi_test.hamastar.com.tw/api/v3/model_test/ABUxIKSFYMEw6ZSUHxF4WA@@/yuykH9slI5prrpqvpS1U7Q@@"
+
+    payload = json.dumps({
+        "articles": [
+            {
+                "Question": get_message
+                }
+            ]
+        })
+    headers = {
+        'Content-Type': 'application/json',
+        'Cookie': '_culture=zh-TW'
+        }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
 
     # Send To Line
-    reply = TextSendMessage(text=f"{get_message}")
+    # reply = TextSendMessage(text=f"{get_message}")
+    reply = TextSendMessage(text=f"{response.text}")
     line_bot_api.reply_message(event.reply_token, reply)
+
